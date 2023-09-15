08ced69b3241982ff7510fe63c52c66fb001c32b
Łukasz <lukasz.poczta12@gmail.com>
2023-09-15 14:47:23 +0200

    removed quotes

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,5 +1,4 @@
 export const QUERY_KEY_WEATHER = 'weatherData';
-// export const QUERY_KEY_FAV = 'weatherData';
 
 export async function fetchWeatherData() {
   const response = await fetch('/api/weather');
@@ -9,14 +8,4 @@ export async function fetchWeatherData() {
   return response.json();
 }
 
-// export async function fetchFavWeatherData(ids: string[]) {
-//   const promises = ids.map(async (id) => {
-//     const response = await fetch(`/api/weather/${id}`);
-//     if (!response.ok) {
-//       throw new Error(`Failed to fetch weather data for ID ${id}`);
-//     }
-//     return response.json();
-//   });
 
-//   return Promise.all(promises);
-// }
